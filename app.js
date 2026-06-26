d026a2a5ed8b5abcb96771363fec3a3fd18b77a2
WellingtonFilho <40251733+WellingtonFilho@users.noreply.github.com>
2026-06-26 16:11:21 -0300

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 function login() {
-  alert("Bem-vindo ao Sistema da Budega do Zé!");
-  window.location.href = "dashboard.html";
+  const usuario = document.querySelector('input[type="text"]').value;
+  const senha = document.querySelector('input[type="password"]').value;
+
+  if (usuario === "" || senha === "") {
+    alert("Preencha usuário e senha!");
+    return;
+  }
+
+  // Login simples (temporário)
+  if (usuario === "admin" && senha === "1234") {
+    alert("Bem-vindo à Budega do Zé!");
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Usuário ou senha inválidos!");
+  }
 }
