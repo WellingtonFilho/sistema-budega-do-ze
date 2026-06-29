e84b634e31f0cd3d78739c0559804b9ab184fda3
WellingtonFilho <40251733+WellingtonFilho@users.noreply.github.com>
2026-06-28 23:28:52 -0300

    assets/js/app.js

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+function entrar() {
+
+    const usuario = document.getElementById("usuario").value;
+    const senha = document.getElementById("senha").value;
+
+    if (usuario === "admin" && senha === "1234") {
+
+        window.location.href = "pages/dashboard.html";
+
+    } else {
+
+        alert("Usuário ou senha incorretos!");
+
+    }
+
+}
